97ae97791d470a17e76c9e067dd47eac6c773e00
Sinthia Rupa <85312355+Sinthia666@users.noreply.github.com>
2021-06-27 00:40:13 +0600

    Add files via upload

---
+++ Arithmetic_Operators_hackerrank.py
@@ -0,0 +1,10 @@
+a = int(input())
+if(a%2!=0 ):
+    print("Weird");
+# elif(a%2 ==0 & 2<=a<=5):
+#     print("Weird");    
+elif(a%2==0 & 6<a<20):
+    print("Weird");
+
+elif(a%2==0 & a<20):
+    print("Not Weird")
